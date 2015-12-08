0506accc7667b86158857d689b43de462362db5f
Notlim <milton__13@hotmail.com>
2015-12-08 20:25:51 -0200

    Primeiro upload
    Enfim acredito que foi dessa vez

---
+++ src/trab/tabelaGrafo.java
@@ -0,0 +1,16 @@
+package trab;
+
+public class tabelaGrafo {
+    private int n;
+
+    public tabelaGrafo() {
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+}
